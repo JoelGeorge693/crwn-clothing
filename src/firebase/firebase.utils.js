29ab54c7c6e847a5e99fd0dcf0eeda8f0e4ce500
JoelGeorge693 <joel.darknight18@gmail.com>
2021-05-08 02:08:57 +0530

    created the signup page and functionality using firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   measurementId: "G-GVG4VHSJTC",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        createdAt,
+        email,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.error(`error creating data ${err.message}`);
+    }
+  }
+
+  return userRef;
+};
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and -sign-up/sign-in-and -sign-up.component.jsx
+++ src/pages/sign-in-and -sign-up/sign-in-and -sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in.components.jsx";
+import SignUp from "../../components/sign-up/sign-up.components.jsx";
 import "./sign-in-and -sign-up.styles.scss";
 
 const SignInAndSignUp = () => (
-  <div className="sign-in-and -sign-up">
+  <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
