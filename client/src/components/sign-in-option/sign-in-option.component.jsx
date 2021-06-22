f239f93aab604dc367675d168edaa029c4acb204
JoelGeorge693 <joel.darknight18@gmail.com>
2021-06-22 23:09:39 +0530

    added a continue as a guest or login option box in homepage using redux and sign-in-option.component.jsx and styles files

--- client/src/components/sign-in-option/sign-in-option.component.jsx
+++ client/src/components/sign-in-option/sign-in-option.component.jsx
@@ -4,17 +4,25 @@ import CustomButton from "../custom-button/custom-button.component";
 import "./sign-in-option.styles.scss";
 import { connect } from "react-redux";
 import hideSignInOption from "../../redux/sign-in-option/sign-in-option.actions";
+import { RemoveButtonContainer } from "../check-out-cart-items/check-out-cart-items.styles";
 
 const SignInOption = ({ history, hideSignInOption }) => (
-  <div className="box">
-    <CustomButton
-      onClick={() => {
-        return history.push("/signin"), hideSignInOption();
-      }}
-    >
-      Sign In
-    </CustomButton>
-    <CustomButton onClick={hideSignInOption}>Continue as a guest</CustomButton>
+  <div className="outer-box">
+    <RemoveButtonContainer className="cross" onClick={hideSignInOption}>
+      &#10005;
+    </RemoveButtonContainer>
+    <div className="box">
+      <CustomButton
+        onClick={() => {
+          return history.push("/signin"), hideSignInOption();
+        }}
+      >
+        Sign In
+      </CustomButton>
+      <CustomButton onClick={hideSignInOption}>
+        Continue as a guest
+      </CustomButton>
+    </div>
   </div>
 );
 
